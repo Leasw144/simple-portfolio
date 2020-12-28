09b36ac7a8577bf53113e238f50a76e8132cc546
Sean Leas <phoenix_24leas.com>
2020-12-28 14:56:03 -0700

    Refactor Projects  to map correctly in Content

--- src/components/gallery/Gallery.js
+++ src/components/gallery/Gallery.js
@@ -1,10 +1,40 @@
 import React from "react";
 import "./Gallery.scss";
 
-export default function Content() {
+export default function Gallery({img, github}) {
+  let stock = 0;
+  const groupedImg = img.map((image) => {
+    stock++;
+    console.log("stock", stock);
+    return (
+      <figure key={stock} className={`gallery__item gallery__item--${stock}`}>
+        <img src={image} alt="first" className="gallery__img" />
+      </figure>
+    );
+  });
+  
+
   return (
-    <div className="">
-      <h1>This is the Gallery of the stuff</h1>
+    <div>
+      <figure className="display">
+        <div className="gallery">
+          {/* <figure className="gallery__item gallery__item--1">
+                <img src={img} alt="first" className="gallery__img" />
+              </figure>
+              <figure className="gallery__item gallery__item--2">
+                <img src={img} alt="second" className="gallery__img" />
+              </figure>
+              <figure className="gallery__item gallery__item--3">
+                <img src={img} alt="third" className="gallery__img" />
+              </figure> */}
+          {groupedImg}
+        </div>
+        <figcaption>
+          <a href={github} target="_blank" rel="noopener noreferrer">
+            link to github
+          </a>
+        </figcaption>
+      </figure>
     </div>
   );
 }
--- src/components/projects/Projects.js
+++ src/components/projects/Projects.js
@@ -1,6 +1,7 @@
 import React from "react";
 import "./Projects.scss";
 import "../../assets/projects/covid1.jpg"
+import Gallery from '../gallery/Gallery.js'
 
 
 export default function Projects({name, img, proj, abstract, tech}) {
@@ -15,44 +16,39 @@ export default function Projects({name, img, proj, abstract, tech}) {
   //     />
   //   );
   // });
-
+  // console.log("these are images", img)
+  // let stock = 0
+  // const groupedImg = img.map(image => {
+  //   stock++
+  //   console.log('stock', stock)
+  //   return (
+  //     <figure key={name} className={`gallery__item gallery__item--${stock}`}>
+  //       <img src={image} alt="first" className="gallery__img" />
+  //     </figure>
+  //   );
+  // })
+  // console.log('grouped', groupedImg)
   const techStack = proj.tech.map(stack => {
     return (
-      <li>{stack}</li>
+      <li key={stack}>{stack}</li>
     )
   })
   return (
     <div>
       <h2>{proj.name}</h2>
       <section className="projDisplay">
-        <div>
-          <figure className="display">
-            <div className="gallery">
-              <figure class="gallery__item gallery__item--1">
-                <img src={img} alt="first" class="gallery__img" />
-              </figure>
-              <figure class="gallery__item gallery__item--2">
-                <img src={img} alt="second" class="gallery__img" />
-              </figure>
-              <figure class="gallery__item gallery__item--3">
-                <img src={img} alt="third" class="gallery__img" />
-              </figure>
-            </div>
-            <figcaption>
-              <a href={proj.github} target="_blank" rel="noopener noreferrer">
-                link to github
-              </a>
-            </figcaption>
-          </figure>
-        </div>
+        <Gallery 
+          img={img}
+          github={proj.github}
+        />
 
         <div className="projSect">
-          <h2>tech stack</h2>
+          <h3>tech stack</h3>
           <ul className="tech">{techStack}</ul>
         </div>
 
         <div className=" projSect abstract">
-          <h2>Abstract</h2>
+          <h3>Abstract</h3>
           <p>{proj.abstract}</p>
         </div>
       </section>
