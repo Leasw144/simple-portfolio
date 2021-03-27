87526ecffc4f10b2d1ec3d793a0bdaf51e14a385
Sean Leas <phoenix_24leas.com>
2021-03-27 15:53:40 -0600

    Revamp nav bar

--- src/components/button/Button.js
+++ src/components/button/Button.js
@@ -3,7 +3,9 @@ import './Button.scss'
 
 export default function Button({id}) {
   return (
-    <a href='project' id={id}>{id}</a>
-  )
+    <a className="navBtn" href="project" id={id}>
+      {id}
+    </a>
+  );
 
 } 
--- src/components/nav/Nav.js
+++ src/components/nav/Nav.js
@@ -1,31 +1,46 @@
 import React from 'react'
 import './Nav.scss'
 import Button from '../button/Button'
+import me from "../../assets/profile/me-bike.jpg";
+import LinkedInIcon from "@material-ui/icons/LinkedIn";
+import GitHubIcon from "@material-ui/icons/GitHub";
+import DescriptionIcon from "@material-ui/icons/Description";
+import PhoneIcon from "@material-ui/icons/Phone";
+import MailIcon from "@material-ui/icons/Mail";
+import IconButton from "@material-ui/core/IconButton";
+import InstagramIcon from "@material-ui/icons/Instagram";
+
+import Tooltip from "@material-ui/core/Tooltip";
 
 export default function Nav() {
   return (
     <div className="navbar">
-      {/* <ul id="grid" class="clear">
-        <li>
-            <div class="hexagon"></div>
-        </li>
-
-      </ul> */}
       <div className="title">
+        <img className="profCircle" src={me} alt="me" />
         {/* <div className="hex">Wattana Leas</div> */}
-        <div className="contact">
+        {/* <div className="contact"> */}
           <h3>Wattana Leas</h3>
-        </div>
+        {/* </div> */}
         {/* <p>omg Linus you're so good at this and this is really impressive - me hyping myself up</p> */}
       </div>
       <div className="controller">
-        <Button location="#home" id="home"/>
-        <Button location="#projects" id="projects"/>
-        <Button location="#about" id="about"/>
-        <Button location="#gallery" id="gallery"/>
+        <Button location="#home" id="home" />
+        <Button location="#projects" id="projects" />
+        <Button location="#about" id="about" />
+        <Button location="#gallery" id="gallery" />
       </div>
-
-
+      <div className="links">
+        <Tooltip title="test" arrow>
+          <LinkedInIcon   />
+        </Tooltip>
+        <Tooltip title="test" arrow>
+          <GitHubIcon   />
+        </Tooltip>
+        <Tooltip title="test" arrow>
+          <InstagramIcon   />
+        </Tooltip>
+      </div>
+     
       {/* <button href="#gallery">Gallery</button>
       <button href="#projects">Projects</button>
       <button href="#experience">experience</button>
