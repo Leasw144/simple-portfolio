33473c9dbdaa811ff226c9c49c7152d12dca6277
Sean Leas <phoenix_24leas.com>
2021-04-25 18:56:11 -0600

    Change a tags to buttons on cards

--- src/components/projectCard/ProjectCard.js
+++ src/components/projectCard/ProjectCard.js
@@ -1,18 +1,62 @@
 import React from "react"
 import "./ProjectCard.scss"
 import Gallery from "../gallery/Gallery.js";
-
+import GitHubIcon from "@material-ui/icons/GitHub";
+import Tooltip from "@material-ui/core/Tooltip";
+import InfoIcon from "@material-ui/icons/Info";
+import FlightTakeoffIcon from "@material-ui/icons/FlightTakeoff";
 
 export default function ProjectCard({key, img, proj}) {
   console.log(key, proj)
+
+
+  const onClick = (e) => {
+    e.preventDefault()
+    console.log("derp")
+  }
   return (
-    <div className="ProjectCard">
-      <header className="project-name">YOOOOO</header>
-      <img className="project-image" src={img[0]} alt={proj.name}/>
+    <div
+      style={{
+        backgroundImage: `url(${img[0]})`,
+      }}
+      className="ProjectCard"
+    >
+      {/* <img className="project-image" src={img[0]} alt={proj.name} /> */}
+      <header className="project-name">{proj.name}</header>
       <footer>
-
+        <Tooltip title="GitHub" arrow>
+          <button
+            className="project-btn"
+            href="https://github.com/Leasw144"
+            target="_blank"
+            rel="noreferrer"
+            alt="Github"
+          >
+            <GitHubIcon className="cursor" color="action" />
+          </button>
+        </Tooltip>
+        <Tooltip title="Info" arrow>
+          <button
+            className="project-btn"
+            rel="noreferrer"
+            alt="Github"
+            onClick={(e) => onClick(e)}
+          >
+            <InfoIcon className="cursor" color="action" />
+          </button>
+        </Tooltip>
+        <Tooltip title="Info" arrow>
+          <button
+            className="project-btn"
+            href="https://github.com/Leasw144"
+            target="_blank"
+            rel="noreferrer"
+            alt="Github"
+          >
+            <FlightTakeoffIcon className="cursor" color="action" />
+          </button>
+        </Tooltip>
       </footer>
-
     </div>
-  )
+  );
 }
